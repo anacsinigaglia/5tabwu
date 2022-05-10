729ba920d35ac0b11ff8a53b57d310ba32bf704f
Ana Caroline Sinigaglia <anacarolinesinigaglia@gmail.com>
2022-05-10 10:53:07 -0300

    Implements local route for questions

---
+++ src/pages/api/questions.ts
@@ -0,0 +1,9 @@
+import { Question } from '../../utils';
+import { questions } from '../../mocks/questions';
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (_req: NextApiRequest, res: NextApiResponse<Question[]>) => {
+  res.status(200).json(questions);
+};
